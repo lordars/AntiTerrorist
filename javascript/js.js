8fc3164829a07b746980d8b37d090e2a8190644b
lordars <adrian.r.scalia@gmail.com>
2022-01-27 03:33:26 -0300

    fundo movimento, player movimento incluido

--- javascript/js.js
+++ javascript/js.js
@@ -1,6 +1,6 @@
 
 
-function start(){
+function start() {
 
     $("#inicio").hide();
 
@@ -9,71 +9,161 @@ function start(){
     $("#fundogame").append("<div id='enimigo2' class='anima3'  ></div> ");
     $("#fundogame").append("<div id='amigo'  class='anima4'></div> ");
 
-    
-    var jogo ={}
+
+
+    var jogo = {}
 
     //movimento dos pessonagems 
-    var tecla={
-        left:37,
-        right:39,
-        up:38,
-        down:40,
-        space:32
+    var tecla = {
+        left: 37,
+        right: 39,
+        up: 38,
+        down: 40,
+        space: 32
     }
-
+    var velocidade = 5;
+    var velocidade2 = 2;
+    var positionY = parseInt(Math.random() * 550);
+    var amigopositionY = parseInt(Math.random() * 550);
+    var vidas = 3;
+    jogo.timer = setInterval(loop, 30);
     jogo.pressionou = [];
 
-    $(document).keydown(function(e){
+
+    $(document).keydown(function (e) {
         jogo.pressionou[e.which] = true;
     })
-    $(document).keyup(function(e){
+    $(document).keyup(function (e) {
         jogo.pressionou[e.which] = false;
     })
-    
+
+
+
+
+  
 
 
 
 
 
-    jogo.timer = setInterval(loop,30);
-    
-    function loop(){
+
+    function loop() {
         moveFundo();
         movejogador();
-    }
-
-        
-    function moveFundo(){
-       
-        esquerda= parseInt($("#fundogame").css("background-position"));
-        $("#fundogame").css("background-position", esquerda-1);
-    }
-
-    function movejogador(){
-        if(jogo.pressionou[tecla.up]){
-            var top0= parseInt($("#jogador").css("top"));
-            if(top0>20 )
-            $("#jogador").css("top", top0-10)
-        }
-        if(jogo.pressionou[tecla.down]){            
-            var down0= parseInt($("#jogador").css("top"));
-            if(down0<550 )
-            $("#jogador").css("top", down0 + 10)
-        }
-        if(jogo.pressionou[tecla.left]){
-            var left0= parseInt($("#jogador").css("left"));
-            if(left0>10 )
-            $("#jogador").css("left", left0-10)
-        }
-        if(jogo.pressionou[tecla.right]){
-            var right0= parseInt($("#jogador").css("left"));
-            if(right0<850 )
-            $("#jogador").css("left", right0 + 10)
-        }
+        moveInimigo();
+        moveAmigo();
     }
 
 
-    
+    function moveFundo() {
+
+        esquerda = parseInt($("#fundogame").css("background-position"));
+        $("#fundogame").css("background-position", esquerda - 1);
+    }
+
+    function movejogador() {
+        if (jogo.pressionou[tecla.up]) {
+            var top0 = parseInt($("#jogador").css("top"));
+            if (top0 > 20)
+                $("#jogador").css("top", top0 - 4)
+        }
+        if (jogo.pressionou[tecla.down]) {
+            var down0 = parseInt($("#jogador").css("top"));
+            if (down0 < 550)
+                $("#jogador").css("top", down0 + 4)
+        }
+        if (jogo.pressionou[tecla.left]) {
+            var left0 = parseInt($("#jogador").css("left"));
+            if (left0 > 10)
+                $("#jogador").css("left", left0 - 4)
+        }
+        if (jogo.pressionou[tecla.right]) {
+            var right0 = parseInt($("#jogador").css("left"));
+            if (right0 < 850)
+                $("#jogador").css("left", right0 + 4)
+        }
+    }
+
+    function moveInimigo() {
+        positionX = parseInt($("#enimigo1").css("left"));
+        $("#enimigo1").css("left", positionX - velocidade);
+        $("#enimigo1").css("top", positionY);
+
+        if (positionX <= 0) {
+            positionY = parseInt(Math.random() * 334)
+            $("#enimigo1").css("left", 764);
+            $("#enimigo1").css("top", positionY);
+        }
+        positionX = parseInt($("#enimigo2").css("left"));
+        $("#enimigo2").css("left", positionX + velocidade2);
+        $("#enimigo2").css("top", amigopositionY);
+
+        if (vidas == 0) {
+
+            $("#fundogame").append("<div id='lost' class='animalost' > </div> ");
+            $("#fundogame").append("<H1 id='die' >YOU Friend DIE, BRO!!</H1>");
+            $("#jogador").remove();
+            $("#enimigo1").remove();
+            $("#enimigo2").remove();
+            $("#amigo").remove();
+            clearInterval(jogo.timer)
+            setTimeout(() => {
+                $("#lost").remove();
+                $("#die").remove();
+                $("#inicio").show();
+                clearInterval()
+            }, 5000);
+
+        }
+
+        if (vidas === 2) {
+           
+                   
+            $("#jogador").css("border-style", "dashed");
+            $("#amigo").css("border-style", "dashed");
+
+            $("#fundogame").append("<div id=morre2 > Vai Morrer!! 2/3</div> ");
+            setTimeout(() => {
+                $("#morre2").remove();
+                
+                clearInterval()
+            }, 10);
+            
+
+        }
+
+        if (vidas === 1) {
+            $("#jogador").css("border-style", "none");
+            $("#amigo").css("border-style", "none");
+            $("#fundogame").append("<div id=morre2 > Vai Morrer!! 1/3</div> ");
+            setTimeout(() => {
+                $("#morre2").remove();
+                
+                clearInterval()
+            }, 10);
+        }
+
+        if (positionX >= 850) {
+            amigopositionY = parseInt(Math.random() * 334)
+            $("#enimigo2").css("left", 0);
+            $("#enimigo2").css("top", amigopositionY);
+            vidas = vidas - 1
+
+        }
+    }
+
+
+    function moveAmigo() {
+        amigopositionX = parseInt($("#amigo").css("left"));
+        $("#amigo").css("top", amigopositionY + velocidade);
+        $("#amigo").css("left", amigopositionX);
+
+        if (positionX <= 0) {
+            amigopositionY = parseInt(Math.random() * 550)
+            $("#amigo").css("top", 764);
+            $("#amigo").css("left", amigopositionX);
+        }
+    }
 
 
 
