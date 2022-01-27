30620313327209df4b4414f430fb21475e3f3048
lordars <adrian.r.scalia@gmail.com>
2022-01-26 23:47:49 -0300

    Subindo parte

---
+++ javascript/js.js
@@ -0,0 +1,11 @@
+
+
+function start(){
+
+    $("#inicio").hide();
+
+    $("#fundogame").append("<div id='jogador'></div> ");
+    $("#fundogame").append("<div id='enimigo1'></div> ");
+    $("#fundogame").append("<div id='enimigo2'></div> ");
+    $("#fundogame").append("<div id='amigo'></div> ");
+}
